38508bed9fc1fa84b49777a3e47330f44da89c39
dualface <dualface@gmail.com>
2014-05-13 10:46:10 +0800

    fix compatibility
    Former-commit-id: 23c3d2d657b52d0e0e8bbb2b57743f593275fae3 [formerly a1d7ae27101bf01207f82906d742032298ff728c]
    Former-commit-id: 226f2968135b9c6bc00ffd3d31e86b743a3283df

--- player/simple_player_mac/run_simple_player_mac.sh
+++ player/simple_player_mac/run_simple_player_mac.sh
@@ -6,11 +6,11 @@ if [ "$QUICK_COCOS2DX_ROOT" == "" ]; then
     return
 fi
 
-PLAYER="$QUICK_COCOS2DX_ROOT/player/mac/player.app/Contents/MacOS/player"
+PLAYER="$QUICK_COCOS2DX_ROOT/player/simple_player_mac/simple_player_mac.app/Contents/MacOS/simple_player_mac"
 CONFIG="-relaunch-off -quick $QUICK_COCOS2DX_ROOT -workdir $DIR"
 
 # set screen size
-SCREEN_SIZE="960x640"
+SCREEN_SIZE="-size 960x640"
 
 CMD="$PLAYER $CONFIG $SCREEN_SIZE"
 until $CMD; do
