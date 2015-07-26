6a6a404c691d7ff6d034095d24dca133c98d0c6a
yinjimmy <jimmy.yin5@gmail.com>
2015-07-27 00:41:35 +0800

    improve android compile speed with -j x
    Former-commit-id: 03073f34d2e05baf34b402661d23dcf328ce38e8 [formerly 14b07efa3ad3e7f14aea705fb66513ab2000c449]
    Former-commit-id: 78a420db4b32d4ee611e7dbedeb1f41e8544401e

--- template/PROJECT_TEMPLATE_01/proj.android/build_native.sh
+++ template/PROJECT_TEMPLATE_01/proj.android/build_native.sh
@@ -36,5 +36,6 @@ cp -rf "$APP_ROOT"/res "$APP_ANDROID_ROOT"/assets/
 
 # build
 echo "Using prebuilt externals"
-"$ANDROID_NDK_ROOT"/ndk-build $ANDROID_NDK_BUILD_FLAGS NDK_DEBUG=$NDK_DEBUG $NDK_BUILD_FLAGS -C "$APP_ANDROID_ROOT" $* \
+"$ANDROID_NDK_ROOT"/ndk-build -j`sysctl -n hw.logicalcpu_max` $ANDROID_NDK_BUILD_FLAGS \
+NDK_DEBUG=$NDK_DEBUG $NDK_BUILD_FLAGS -C "$APP_ANDROID_ROOT" $* \
 "NDK_MODULE_PATH=${QUICK_COCOS2DX_ROOT}:${COCOS2DX_ROOT}:${COCOS2DX_ROOT}/cocos2dx/platform/third_party/android/prebuilt"
