da8ee381f2e7026604d0934ed3da46f24f7acbbf
yinjimmy <jimmy.yin5@gmail.com>
2015-07-31 23:03:39 +0800

    remove url tracking with player

--- player/sources/AppDelegate.cpp
+++ player/sources/AppDelegate.cpp
@@ -120,21 +120,6 @@ void AppDelegate::trackEvent(const char *eventName)
 void AppDelegate::trackLaunchEvent()
 {
     trackEvent("launch");
-    
-    const char *trackUrl = nullptr;
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
-    trackUrl = "http://c.kp747.com/k.js?id=c19010907080b2d7";
-#elif (CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
-    trackUrl = "http://c.kp747.com/k.js?id=c1e060d0a0e0e207";
-#endif
-    
-    if (trackUrl)
-    {
-        CCHTTPRequest *request = CCHTTPRequest::createWithUrl(NULL,
-                                                              trackUrl,
-                                                              kCCHTTPRequestMethodGET);
-        request->start();
-    }
 }
 // ----------------------------------------
 
