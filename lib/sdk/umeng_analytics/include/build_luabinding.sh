16526740d1dc1648509f0b59e8b65e962b7c65e7
yinjimmy <jimmy.yin5@gmail.com>
2015-07-31 10:40:37 +0800

    remove among cocos{ad,push}

--- lib/sdk/umeng_analytics/include/build_luabinding.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$DIR"
-OUTPUT_DIR="$DIR"
-MAKE_LUABINDING="$QMM_ROOT"/bin/compile_luabinding.sh
-$MAKE_LUABINDING -E MyClass -d "$OUTPUT_DIR" MobClickCppForLua_luabinding.tolua
--- samples/samples.lua
+++ samples/samples.lua
@@ -16,20 +16,6 @@ local data =
     },
 
     {
-        title       = "chukong_ad",
-        description = "触控广告 SDK 集成（真机）",
-        path        = "samples/chukong_ad",
-        image       = "chukong_ad_ss.png"
-    },
-
-    {
-        title       = "cocopush",
-        description = "触控 Push SDK 集成（真机）",
-        path        = "samples/cocopush",
-        image       = "cocopush_ss.png"
-    },
-
-    {
         title       = "coinflip",
         description = "一个翻转金币的小游戏",
         path        = "samples/coinflip",
@@ -125,41 +111,6 @@ local data =
         description = "UI 系统测试",
         path        = "samples/ui",
         image       = "ui_ss.png"
-    },
-
-    {
-        title       = "umeng_analytics",
-        description = "友盟统计 SDK（真机）",
-        path        = "samples/umeng_analytics",
-        image       = "umeng_analytics_ss.png"
-    },
-
-    {
-        title       = "umeng_feedback",
-        description = "友盟反馈 SDK（真机）",
-        path        = "samples/umeng_feedback",
-        image       = "umeng_feedback_ss.png"
-    },
-
-    {
-        title       = "umeng_push",
-        description = "友盟 Push SDK（真机）",
-        path        = "samples/umeng_push",
-        image       = "umeng_push_ss.png"
-    },
-
-    {
-        title       = "umeng_share",
-        description = "友盟分享 SDK（真机）",
-        path        = "samples/umeng_share",
-        image       = "umeng_share_ss.png"
-    },
-
-    {
-        title       = "umeng_update",
-        description = "友盟更新 SDK（真机）",
-        path        = "samples/umeng_update",
-        image       = "umeng_update_ss.png"
     },
 
     {
